060891684d09687bb86ae785a032ba939cc0d5e8
DE-IGNIS <chauhansudarsh@gmail.com>
2026-05-20 23:33:59 +0530

    Config : background loaction permission included andorid+ios

---
+++ mobile/app/(tabs)/anomaly.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+function Anomaly() {
+  return (
+    <View>
+      <Text>Surveillance Active</Text>
+      <Text>Anomaly Feed</Text>
+    </View>
+  );
+}
+
+export default Anomaly;
