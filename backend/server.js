4102082cfdad39ef25123db74fe1cf44e8104027
DE-IGNIS <chauhansudarsh@gmail.com>
2026-05-22 11:50:51 +0530

    init : initial backend files setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import transactionRoutes from './routes/transactionRoutes.js';
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/transactions', transactionRoutes);
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
