17c360f468e36480b18aee18fc808a9d4bc54a9b
DE-IGNIS <chauhansudarsh@gmail.com>
2026-06-03 11:20:58 +0530

    api: included /api/transactions api endpoint and new route for fetching alerts

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -2,7 +2,8 @@ import express from 'express';
 import {
   getTransactions,
   addTransaction,
-  getTransactionCount
+  getTransactionCount,
+  getAlertTransactions
 } from '../controllers/transactionController.js';
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get('/getTransactions', getTransactions);
 router.get('/getTransactionCount', getTransactionCount);
 router.post('/addTransactions', addTransaction);
+router.get('/alerts', getAlertTransactions)
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', transactionRoutes);
+app.use('/api/transactions', transactionRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
